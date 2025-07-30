cd4c0891836c071c7b5618494c82a9d10cf94310
yohannaaaa <yohannamerawi@gmail.com>
2025-07-30 17:06:40 +0300

    User repository

--- Domain/interface.go
+++ Domain/interface.go
@@ -1,1 +1,6 @@
 package domain
+type UserRepository interface {
+	Create(user *User) error
+	Login(email, password string) (*User, error) //authenticate and return user
+	FetchByEmail(email string) (bool, error) //checks if user exisits or not
+}
