49d5e8646499f90e242f6deee56ee50c4f79f896
salemhabte <habtesalem20@gmail.com>
2025-07-31 11:31:33 +0300

    work on registration

--- Delivery/controllers/controller.go
+++ Delivery/controllers/controller.go
@@ -1,1 +1,43 @@
 package controllers
+
+import (
+	
+	"net/http"
+	"blog_starter_project_g66/Usecases"
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	UserUsecase *usecases.UserUsecase
+}
+
+func NewUserUsecase(uuc *usecases.UserUsecase) *UserController{
+	return &UserController{
+		UserUsecase: uuc,
+	}
+}
+func (uc *UserController) Registration(ctx *gin.Context){
+
+	var user *UserDTO
+
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusNotFound,gin.H{
+			"error": "Invalid request payload",
+		} )
+		return
+	}
+	if user.Email == "" || user.Password == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid request payload"})
+		return
+	}
+	err := uc.UserUsecase.HandleRegistration()
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "successuly created",
+	})
+}
--- Domain/interface.go
+++ Domain/interface.go
@@ -1,6 +1,14 @@
 package domain
-type UserRepository interface {
+
+type IUserRepository interface {
 	Create(user *User) error
 	Login(email, password string) (*User, error) //authenticate and return user
-	FetchByEmail(email string) (bool, error) //checks if user exisits or not
+	FetchByEmail(email string) (bool, error)     //checks if user exisits or not
+}
+
+type IUserValidation interface {
+	IsValidEmail(email string) bool
+	IsStrongPassword(password string) bool
+	Hashpassword(password string) string
+	ComparePassword(userPassword, password string) error
 }
