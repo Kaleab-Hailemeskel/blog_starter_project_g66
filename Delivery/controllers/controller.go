37c1907a697dde22f275c2d588cda91aee452cf8
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-08-01 15:50:01 +0300

    folder structure changed; dto, domain, interface are now in Domain package

--- Delivery/controllers/controller.go
+++ Delivery/controllers/controller.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	domain "blog_starter_project_g66/Domain"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Controller struct {
+	BlogUseCase domain.IBlogUseCase
+	// UserUseCase domain.IUserUseCase  //! For the time being it is commented out
+}
+
+func NewController(blogUseCase domain.IBlogUseCase) *Controller {
+	return &Controller{
+		BlogUseCase: blogUseCase,
+	}
+}
+
+func (cntrl *Controller) FilterBlog(ctx *gin.Context) {
+	ctx.Query("")
+}
