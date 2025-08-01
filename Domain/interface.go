0fd94462e0d48e18613a9097e2bf6b16c3ebe987
salemhabte <habtesalem20@gmail.com>
2025-08-01 17:19:21 +0300

    work on otp

--- Domain/interface.go
+++ Domain/interface.go
@@ -2,11 +2,10 @@ package domain
 
 
 type IUserRepository interface {
-	Create(username string,user *User) error
+	Create(username string, user *User) error
 	Login(email, password string) (*User, error) //authenticate and return user
 	FetchByEmail(email string) (bool, error)     //checks if user exisits or not
 }
-
 
 type IUserValidation interface {
 	IsValidEmail(email string) bool
@@ -14,15 +13,8 @@ type IUserValidation interface {
 	Hashpassword(password string) string
 	ComparePassword(userPassword, password string) error
 }
-
-type IUserOTP interface{
+type IUserOTP interface {
 	StoreOTP(entry UserUnverified) error
 	FindOTP(email string) (*UserUnverified, error)
 	DeleteOTP(email string) error
-
-}
-
-type GenerateOTP interface{
-	GenerateRandomOTP() string 
-	Send(toEmail, otp string) error
 }
