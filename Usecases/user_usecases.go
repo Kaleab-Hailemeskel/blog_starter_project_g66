5fc677e4cb0f24fd778fd5ecf1ea4e40833cf66a
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-08-04 09:53:27 +0300

    UserUseCase has Demote and promote func

--- Usecases/user_usecases.go
+++ Usecases/user_usecases.go
@@ -1,1 +1,19 @@
 package usecases
+
+import domain "blog_starter_project_g66/Domain"
+
+type UserUseCase struct {
+	UserDataBase domain.IUserRepository
+}
+
+func NewUserUseCase(blogDB domain.IBlogRepository, userDB domain.IUserRepository) *UserUseCase {
+	return &UserUseCase{
+		UserDataBase: userDB,
+	}
+}
+func (uuc *UserUseCase) DemoteUser(userEmail string) error {
+	return uuc.UserDataBase.DemoteUser(userEmail)
+}
+func (uuc *UserUseCase) PromoteUser(userEmail string) error {
+	return uuc.UserDataBase.PromoteUser(userEmail)
+}
