af7c8e5e2630c1178b0aa9fcec45354ba46b7d76
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-08-07 12:02:09 +0300

    FindBlogByID now returns BlogDTO instead of Blog

--- Domain/interface.go
+++ Domain/interface.go
@@ -50,7 +50,7 @@ type IEmailService interface {
 }
 type IBlogRepository interface {
 	CreateBlog(blog *Blog, userID primitive.ObjectID) error
-	FindBlogByID(blogID primitive.ObjectID) (*Blog, error)
+	FindBlogByID(blogID primitive.ObjectID) (*BlogDTO, error)
 	DeleteBlogByID(blogID primitive.ObjectID) error
 	UpdateBlogByID(blogID primitive.ObjectID, updatedBlog *Blog) error
 	// page number needed for the purpose of pagination
@@ -77,6 +77,7 @@ type IBlogUseCase interface {
 	CreateBlog(blog *Blog, userEmail string) error //! Instead of userEmail as string we can pass userID instantly
 	DeleteBlogByID(blogID string) error            // the controller will pass the a string from the url the usecase will change it to the objectID
 	UpdateBlogByID(blogID string, updatedBlog *Blog) error
+	GetBlogByID(blogID primitive.ObjectID) (*BlogDTO, error)
 	// page number needed for the purpose of pagination
 	GetAllBlogsByFilter(url_filter *Filter, pageNumber int) ([]*BlogDTO, error)
 	LikeBlog(blogID primitive.ObjectID, userEmail string) error
