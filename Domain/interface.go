8ff2f6bd42ef119ef506e1963833d4fa12ecd3d1
Kaleab Hailemeskel <kaleabhailemeskel47@gmail.com>
2025-08-10 22:24:58 +0300

    commiting after merge

--- Domain/interface.go
+++ Domain/interface.go
@@ -55,7 +55,6 @@ type IEmailService interface {
 type IBlogRepository interface {
 	IsClientConnected() bool // just for testing on the testify purpose
 	CreateBlog(blog *Blog, userID primitive.ObjectID) (*BlogDTO, error)
-	CreateBlog(blog *Blog, userID primitive.ObjectID) (*Blog, error)
 	FindBlogByID(blogID primitive.ObjectID) (*BlogDTO, error)
 	DeleteBlogByID(blogID primitive.ObjectID) error
 	UpdateBlogByID(blogID primitive.ObjectID, updatedBlog *Blog) error
